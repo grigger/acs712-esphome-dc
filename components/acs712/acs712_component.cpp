f37a3eb29fc0aa49a12064cb316a1e412c595573
George Grigore <geo.grig@gmail.com>
2026-03-10 23:50:54 +0000

    Add AC/DC functionality to cpp

--- components/acs712/acs712_component.cpp
+++ components/acs712/acs712_component.cpp
@@ -3,23 +3,45 @@
 namespace esphome {
 namespace acs712 {
 
+static const char *const TAG = "acs712";
+
 void ACS712Sensor::setup() {
   this->set_update_interval(3000);
-  acs_.autoMidPointDC(100);
-  ESP_LOGD("acs712", "MidPoint: %d", acs_.getMidPoint());
-  ESP_LOGD("acs712", "Noise mV: %d", acs_.getNoisemV());
+  
+  if (this->is_ac_) {
+    this->acs_.autoMidPoint(50, 4);
+    ESP_LOGCONFIG(TAG, "ACS712 mode: AC");
+  } else {
+    this->acs_.autoMidPointDC(100);
+    ESP_LOGCONFIG(TAG, "ACS712 mode: DC");
+  }
+
+  ESP_LOGCONFIG(TAG, "  Pin: %u", this->_pin);
+  ESP_LOGCONFIG(TAG, "  MidPoint: %u", this->acs_.getMidPoint());
+  ESP_LOGCONFIG(TAG, "  Noise mV: %u", this->acs_.getNoisemV());
 }
 
 void ACS712Sensor::update() {
   float average = 0;
   int count = 100;
-  for (int i = 0; i < count; i++) {
-    average += acs_.mA_DC();
+  float amps;
+
+  if (this->is_ac_) {
+    amps = this->acs_.mA_AC(50, 4);   // Europe mains
+  } else {
+    // for (int i = 0; i < count; i++) {
+    //   average += acs_.mA_DC();
+    // }
+    
+    // amps = average / count / 1000.0;
+    amps = this->acs_.mA_DC(100);
   }
-  float amps = average / count / 1000.0;
+
+  float sensor_output_v = analogReadMilliVolts(this->pin_) / 1000.0f;
+  
   current_sensor->publish_state(amps);
   power_sensor->publish_state(amps * line_voltage_);
-  voltage_sensor->publish_state(average);
+  voltage_sensor->publish_state(sensor_output_v);
 }
 
 }  // namespace acs712
