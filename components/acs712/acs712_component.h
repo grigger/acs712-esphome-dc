923cf8e1ec9f5fcfa9ba03926bf0e74116a61d18
George Grigore <geo.grig@gmail.com>
2026-03-11 00:06:02 +0000

    Update acs712_component.h: Add absolute as config

--- components/acs712/acs712_component.h
+++ components/acs712/acs712_component.h
@@ -14,10 +14,11 @@ class ACS712Sensor : public PollingComponent {
    public:
     ACS712Sensor(uint8_t pin, float voltage, uint16_t adc_bits, float mV_per_amp, float line_voltage)
         : PollingComponent(3000),
-           pin_(pin),
            acs_(pin, voltage, adc_bits, mV_per_amp),
+           pin_(pin),
            line_voltage_(line_voltage),
            is_ac_(false),
+           absolute_(false),
            current_sensor(nullptr),
            power_sensor(nullptr),
            voltage_sensor(nullptr) {}
@@ -28,6 +29,7 @@ class ACS712Sensor : public PollingComponent {
     void set_is_ac(bool is_ac) { this->is_ac_ = is_ac; }
     void set_noisemV(float noisemV) { acs_.setNoisemV(noisemV); }
     void set_mid_point(uint16_t mid_point) { acs_.setMidPoint(mid_point); }
+    void set_absolute(bool absolute) { absolute_ = absolute; }
 
     // Nuevos setters para los sensores configurados desde YAML
     void set_current_sensor(sensor::Sensor *sensor) { current_sensor = sensor; }
@@ -39,6 +41,7 @@ class ACS712Sensor : public PollingComponent {
     uint8_t pin_;
     float line_voltage_;
     bool is_ac_;
+    bool absolute_;
     sensor::Sensor *current_sensor;
     sensor::Sensor *power_sensor;
     sensor::Sensor *voltage_sensor;
