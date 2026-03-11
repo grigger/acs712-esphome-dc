1f8cc5ab7da0f21cf2af523a124153ad9785e84c
George Grigore <geo.grig@gmail.com>
2026-03-11 02:50:31 +0000

    Update acs712_component.h

--- components/acs712/acs712_component.h
+++ components/acs712/acs712_component.h
@@ -19,6 +19,12 @@ class ACS712Sensor : public PollingComponent {
            line_voltage_(line_voltage),
            is_ac_(false),
            absolute_(false),
+           manual_midpoint_set_(false),
+           manual_noise_set_(false),
+           samples_dc(100),
+           samples_ac(4),
+           freq_dc(1000),
+           freq_ac(4),
            current_sensor(nullptr),
            power_sensor(nullptr),
            voltage_sensor(nullptr) {}
@@ -28,8 +34,18 @@ class ACS712Sensor : public PollingComponent {
     void update() override;
 
     void set_is_ac(bool is_ac) { this->is_ac_ = is_ac; }
-    void set_noisemV(float noisemV) { acs_.setNoisemV(noisemV); }
-    void set_mid_point(uint16_t mid_point) { acs_.setMidPoint(mid_point); }
+    void set_noisemV(float noisemV) { 
+       acs_.setNoisemV(noisemV);
+       this->manual_noise_set_ = true;
+    }
+    void set_noiseSupress(bool flag) { 
+       acs_.supressNoise(flag); 
+       this->noise_suppress_ = flag;
+    }
+    void set_mid_point(uint16_t mid_point) { 
+       acs_.setMidPoint(mid_point); 
+       this->manual_midpoint_set_ = true;
+    }
     void set_absolute(bool absolute) { absolute_ = absolute; }
 
     // Nuevos setters para los sensores configurados desde YAML
@@ -43,6 +59,13 @@ class ACS712Sensor : public PollingComponent {
     float line_voltage_;
     bool is_ac_;
     bool absolute_;
+    bool manual_midpoint_set_;
+    bool manual_noise_set_;
+    bool noise_suppress_;
+    uint8_t samples_dc;
+    uint8_t samples_ac;
+    uint8_t freq_dc;
+    uint8_t freq_ac;
     sensor::Sensor *current_sensor;
     sensor::Sensor *power_sensor;
     sensor::Sensor *voltage_sensor;
