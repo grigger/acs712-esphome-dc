ea176172de21e3ef8f106b60d0863e3ff223bad3
George Grigore <geo.grig@gmail.com>
2026-03-11 00:12:01 +0000

    Add WCS1700 sensor.py

---
+++ components/wcs1700/sensor.py
@@ -0,0 +1,107 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import (
+    CONF_ID, CONF_PIN, CONF_VOLTAGE, UNIT_AMPERE, UNIT_WATT, UNIT_VOLT,
+    ICON_CURRENT_AC, ICON_FLASH,
+    DEVICE_CLASS_CURRENT, DEVICE_CLASS_POWER, DEVICE_CLASS_VOLTAGE
+)
+
+# Agregamos la dependencia a la biblioteca ACS712
+cg.add_library(
+    name="ACS712",
+    repository="https://github.com/RobTillaart/ACS712.git",
+    version=">=0.3.0"
+)
+
+CONF_SENSOR_TYPE = "type"
+TYPE_AC = "AC"
+TYPE_DC = "DC"
+
+DEPENDENCIES = []
+#MULTI_CONF = True
+
+# Nombres de los nuevos campos para los sensores hijos
+CONF_CURRENT_SENSOR = "current_sensor"
+CONF_POWER_SENSOR = "power_sensor"
+CONF_VOLTAGE_SENSOR = "voltage_sensor"
+
+# Constantes de configuración originales
+CONF_ADC_BITS = "adc_bits"
+CONF_MV_PER_AMP = "mv_per_amp"
+CONF_LINE_VOLTAGE = "line_voltage"
+CONF_NOISE_MV = "noisemV"
+CONF_MID_POINT = "mid_point"
+CONF_ABSOLUTE = "absolute"
+
+# Define el namespace sin puntos
+wcs1700_ns = cg.esphome_ns.namespace("wcs1700_ns")
+WCS1700Sensor = wcs1700_ns.class_("WCS1700Sensor", cg.PollingComponent)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(WCS1700Sensor),
+    cv.Required(CONF_PIN): cv.int_,
+    cv.Required(CONF_VOLTAGE): cv.float_,
+    cv.Required(CONF_ADC_BITS): cv.int_,
+    cv.Required(CONF_MV_PER_AMP): cv.float_,
+    cv.Required(CONF_LINE_VOLTAGE): cv.float_,
+    cv.Optional(CONF_NOISE_MV, default=43): cv.float_,
+    cv.Optional(CONF_SENSOR_TYPE, default=TYPE_DC): cv.one_of(TYPE_AC, TYPE_DC, upper=True),
+    cv.Optional(CONF_MID_POINT): cv.int_,
+    cv.Optional(CONF_ABSOLUTE, default=False): cv.boolean,
+    
+    # Se definen los schemas para los sensores internos
+    cv.Optional(CONF_CURRENT_SENSOR): sensor.sensor_schema(
+            unit_of_measurement=UNIT_AMPERE,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_CURRENT,
+            icon=ICON_CURRENT_AC,
+    ),
+    cv.Optional(CONF_POWER_SENSOR): sensor.sensor_schema(
+            unit_of_measurement=UNIT_WATT,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_POWER,
+            icon=ICON_FLASH,
+    ),
+    cv.Optional(CONF_VOLTAGE_SENSOR): sensor.sensor_schema(
+            unit_of_measurement=UNIT_VOLT,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_VOLTAGE,
+            icon="mdi:sine-wave"
+    ),
+}).extend(cv.polling_component_schema("3s"))
+
+async def to_code(config):
+    var = cg.new_Pvariable(
+        config[CONF_ID],
+        config[CONF_PIN],
+        config[CONF_VOLTAGE],
+        config[CONF_ADC_BITS],
+        config[CONF_MV_PER_AMP],
+        config[CONF_LINE_VOLTAGE]
+    )
+    await cg.register_component(var, config)
+    
+    cg.add(var.set_is_ac(config[CONF_SENSOR_TYPE] == TYPE_AC))
+    
+    cg.add(var.set_absolute(config[CONF_ABSOLUTE]))
+    
+    if CONF_NOISE_MV in config:
+        cg.add(var.set_noisemV(config[CONF_NOISE_MV]))
+    if CONF_MID_POINT in config:
+        cg.add(var.set_mid_point(config[CONF_MID_POINT]))
+    
+    # Registra el sensor de corriente (amperes) si se ha definido en el YAML
+    if CONF_CURRENT_SENSOR in config:
+        current_sensor = await sensor.new_sensor(config[CONF_CURRENT_SENSOR])
+        cg.add(var.set_current_sensor(current_sensor))
+    
+    # Registra el sensor de potencia (watts) si se ha definido en el YAML
+    if CONF_POWER_SENSOR in config:
+        power_sensor = await sensor.new_sensor(config[CONF_POWER_SENSOR])
+        cg.add(var.set_power_sensor(power_sensor))
+    
+    # Registra el sensor de potencia (watts) si se ha definido en el YAML
+    if CONF_VOLTAGE_SENSOR in config:
+        voltage_sensor = await sensor.new_sensor(config[CONF_VOLTAGE_SENSOR])
+        cg.add(var.set_voltage_sensor(voltage_sensor))
