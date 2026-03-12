ced2f89fd3c0750ea7d2f41bbea8f690e7e1024b
George Grigore <geo.grig@gmail.com>
2026-03-12 19:48:32 +0000

    Update sensor.py

--- components/acs712/sensor.py
+++ components/acs712/sensor.py
@@ -75,7 +75,7 @@ CONFIG_SCHEMA = cv.Schema({
             device_class=DEVICE_CLASS_VOLTAGE,
             icon="mdi:sine-wave"
     ),
-}).extend(cv.polling_component_schema("3s"))
+}).extend(cv.polling_component_schema("10s"))
 
 async def to_code(config):
     var = cg.new_Pvariable(
